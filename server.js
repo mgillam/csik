67c9c29f551ff4f8ddc489f663be91a4fc1a965b
Mic <mic@secureideas.com>
2019-07-06 21:20:09 -0400

    Eslint and server.js cleanuo

--- server.js
+++ server.js
@@ -28,8 +28,9 @@ app.get('/loaders', (req, res) => {
 app.get('/loaders/without/:filter', (req, res) => {
   const blacklist = req.params.filter
   return res.json(loaders.list().reduce((loaderList, key) => {
-    if (loaders.get(key).charset.allowedByBlacklist(blacklist))
-      loaderList.push(key) 
+    if (loaders.get(key).charset.allowedByBlacklist(blacklist)) {
+      loaderList.push(key)
+    }
     return loaderList
   }, []))
 })
@@ -37,18 +38,19 @@ app.get('/loaders/without/:filter', (req, res) => {
 app.get('/loaders/madeof/:filter', (req, res) => {
   const whitelist = req.params.filter
   return res.json(loaders.list().reduce((loaderList, key) => {
-    if (loaders.get(key).charset.allowedByWhiteList(whitelist))
+    if (loaders.get(key).charset.allowedByWhiteList(whitelist)) {
       loaderList.push(key)
+    }
     return loaderList
   }, []))
 })
 
 app.get('/loader/:loader', (req, res) => {
-    // May need to be text/javascript for CVE-2015-9251
-    res.set('Content-Type', 'application/javascript')
-    return res.send(
-      loaders.get(req.params.loader).script({ PAYLOAD_URL: payloadUrl })
-    )
+  // May need to be text/javascript for CVE-2015-9251
+  res.set('Content-Type', 'application/javascript')
+  return res.send(
+    loaders.get(req.params.loader).script({ PAYLOAD_URL: payloadUrl })
+  )
 })
 
 app.get('/payload/:payload', (req, res) => {
@@ -62,19 +64,19 @@ io.on('connection', (socket) => {
 
 server.listen(port, () => console.log(`CSIK Server listening on port ${port}!`))
 
-async function loadLoaders() {
+async function loadLoaders () {
   let loaders = await listDir(resolve('./loaders/'))
   let loaderMap = {}
-  while(loaders.length > 0) {
+  while (loaders.length > 0) {
     let loader = loaders.shift()
     ld.set(loaderMap, ld.trimEnd(loader, '.js'), await import(`./loaders/${loader}`))
   }
   return {
-      get: (key) => {
+    get: (key) => {
       return loaderMap[key] ? loaderMap[key].default : 'Not a loader'
-      },
-      list: () => {
-        return Object.keys(loaderMap)
-      }
+    },
+    list: () => {
+      return Object.keys(loaderMap)
+    }
   }
 }
