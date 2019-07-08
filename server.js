4e8ebd49cad2c96863600a3233aadbd33af40ddd
Mic <mic@secureideas.com>
2019-07-07 20:45:24 -0400

    Server code reorg

--- server.js
+++ server.js
@@ -6,15 +6,9 @@ import attackRouter from './routers/attack.mjs'
 import clientRouter from './routers/client.mjs'
 
 const attackApi = express()
-const managementApi = express()
 const attackServer = http.createServer(attackApi)
-const managementServer = http.createServer(managementApi)
-
 const attackSocket = socketio(attackServer)
 const attackPort = 3000
-
-const managementSocket = socketio(managementServer)
-const managementPort = 3001
 
 attackApi.use(cors())
 attackApi.use('/atk', attackRouter)
@@ -25,8 +19,12 @@ attackSocket.on('connection', (socket) => {
   console.log('Target connection received')
   socket.emit('run', 'alert("xss")')
 })
-
 attackServer.listen(attackPort, () => console.log(`CSIK Server listening on port ${attackPort}!`))
+
+const managementApi = express()
+const managementServer = http.createServer(managementApi)
+const managementSocket = socketio(managementServer)
+const managementPort = 3001
 
 managementApi.use(cors())
 managementApi.use('/atk', attackRouter)
@@ -34,5 +32,4 @@ managementApi.use('/api', clientRouter)
 managementSocket.on('connection', (socket) => {
   console.log('Client connected')
 })
-
 managementServer.listen(managementPort, 'localhost', () => console.log(`Management interface listening on port ${managementPort}`))
