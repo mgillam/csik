6cf24c6daedd9bbf65d2e061fcd97cd06ac03c73
Mic <mic@secureideas.com>
2019-07-07 20:33:06 -0400

    Created management interface

--- server.js
+++ server.js
@@ -5,23 +5,34 @@ import cors from 'cors'
 import attackRouter from './routers/attack.mjs'
 import clientRouter from './routers/client.mjs'
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
-const port = 3000
+const attackApi = express()
+const managementApi = express()
+const attackServer = http.createServer(attackApi)
+const managementServer = http.createServer(managementApi)
 
-app.use(cors())
+const attackSocket = socketio(attackServer)
+const attackPort = 3000
 
-app.use('/api', clientRouter)
-app.use('/atk', attackRouter)
+const managementSocket = socketio(managementServer)
+const managementPort = 3001
 
-app.get('/:alias', (req, res) => {
+attackApi.use(cors())
+attackApi.use('/atk', attackRouter)
+attackApi.get('/:alias', (req, res) => {
   res.send('Getting alias: ' + req.params.alias)
 })
-
-io.on('connection', (socket) => {
-  console.log('Connection received')
+attackSocket.on('connection', (socket) => {
+  console.log('Target connection received')
   socket.emit('run', 'alert("xss")')
 })
 
-server.listen(port, () => console.log(`CSIK Server listening on port ${port}!`))
+attackServer.listen(attackPort, () => console.log(`CSIK Server listening on port ${attackPort}!`))
+
+managementApi.use(cors())
+managementApi.use('/atk', attackRouter)
+managementApi.use('/api', clientRouter)
+managementSocket.on('connection', (socket) => {
+  console.log('Client connected')
+})
+
+managementServer.listen(managementPort, 'localhost', () => console.log(`Management interface listening on port ${managementPort}`))
