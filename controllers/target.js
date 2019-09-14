ae38402e753c5df0f55ae6cb5c2d61c9e5a89f6d
Mic <mic@secureideas.com>
2019-09-14 10:33:15 -0400

    Target deploy endpoint now delivers actual payloads from

--- controllers/target.js
+++ controllers/target.js
@@ -1,11 +1,15 @@
 const attackSocket = require('../serverInterfaces/attackSocket')
+const { resolve } = require('path')
+const fs = require('fs')
 
 const controller = {
   list: (req, res) => {
     res.json(Object.keys(attackSocket.of('/').connected))
   },
   deploy: (req, res) => {
-    // TODO: Drop payload on target
+    let payload = fs.readFileSync(resolve('./payloads/' + req.params.payload + '.js'), { encoding: 'utf8' })
+    console.log(attackSocket.of('/').connected[req.params.target].emit('run', payload))
+    res.json({})
   }
 }
 
